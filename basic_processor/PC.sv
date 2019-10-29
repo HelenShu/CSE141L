5a2ded8648c1609d1b3c980fa62afbc598ff3737
ald031 <38924629+ald031@users.noreply.github.com>
2019-10-29 11:17:48 -0700

    Removed Branch relative/ Branch_re1 signal.

--- basic_processor/PC.sv
+++ basic_processor/PC.sv
@@ -5,7 +5,6 @@
 // issues halt when PC reaches 63
 module PC(
  	input Branch_abs,		      // jump to Target value	   
- 	input Branch_rel_en,		  // jump to Target + PC
  	input ALU_zero,			  // flag from ALU
  	input [15:0] Target,		  // jump ... "how high?"
  	input Init,				  // reset, start, etc. 
@@ -21,8 +20,6 @@ module PC(
 	  PC <= PC;
 	else if(Branch_abs)	      // unconditional absolute jump
 	  PC <= Target;
-	else if(Branch_rel_en && ALU_zero) // conditional relative jump
-	  PC <= Target + PC;
 	else
 	  PC <= PC + 1;		      // default increment (no need for ARM/MIPS +4 -- why?)
 
